63992d741217f41899515b2b15e35c91e1170149
wwf9w0 <aliemrekaradag1@gmail.com>
2022-04-10 14:42:05 +0300

    fixed

--- src/main/java/com/userservice/persistence/jpa/repository/UserProfileRepository.java
+++ src/main/java/com/userservice/persistence/jpa/repository/UserProfileRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long> {
     Optional<UserProfileEntity> findByUserName(String username);
 }
